5b1f2c65d6b4d6474cbcebd05b01cd44ae57b974
Paralleltree <paralleltree@outlook.com>
2018-02-04 12:45:22 +0900

    検証用クラス追加

--- Ched/Components/Exporter/IExporter.cs
+++ Ched/Components/Exporter/IExporter.cs
@@ -24,6 +24,13 @@ namespace Ched.Components.Exporter
         /// <param name="path">エクスポート先のパス</param>
         /// <param name="book">譜面データ</param>
         void Export(string path, ScoreBook book);
+
+        /// <summary>
+        /// 入力された譜面データに対して検証を行い結果を返します。
+        /// </summary>
+        /// <param name="book">検証を行う譜面データ</param>
+        /// <returns>検証結果を格納した<see cref="IEnumerable{DiagnosticData}"/></returns>
+        IEnumerable<DiagnosticData> GetDiagnostics(ScoreBook book);
     }
 
     /// <summary>
@@ -33,5 +40,48 @@ namespace Ched.Components.Exporter
     public interface IExtendedExpoerter<TArgs> : IExporter
     {
         TArgs CustomArgs { get; set; }
+    }
+
+    /// <summary>
+    /// 譜面データに対する検証結果を表します。
+    /// </summary>
+    public class DiagnosticData
+    {
+        /// <summary>
+        /// この<see cref="DiagnosticData"/>に対する重大度を取得します。
+        /// </summary>
+        public DiagnosticSeverity Severity { get; }
+
+        /// <summary>
+        /// この<see cref="DiagnosticData"/>に対する説明を取得します。
+        /// </summary>
+        public string Description { get; }
+
+        public DiagnosticData(DiagnosticSeverity severity, string description)
+        {
+            Severity = severity;
+            Description = description;
+        }
+    }
+
+    /// <summary>
+    /// <see cref="DiagnosticData"/>に対する重大度を表します。
+    /// </summary>
+    public enum DiagnosticSeverity
+    {
+        /// <summary>
+        /// エクスポート上問題にならない情報を表します。
+        /// </summary>
+        Information,
+
+        /// <summary>
+        /// エクスポート上問題がある状態を表します。
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// エクスポートできない状態を表します。
+        /// </summary>
+        Error,
     }
 }
