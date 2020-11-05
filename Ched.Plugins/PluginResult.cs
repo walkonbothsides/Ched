90d2dead8e55c828c35be90368b7d556682867da
Paralleltree <paralleltree@outlook.com>
2020-11-05 15:57:54 +0900

    Add PluginResult

---
+++ Ched.Plugins/PluginResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ched.Plugins
+{
+    /// <summary>
+    /// プラグインの処理結果を表します。
+    /// </summary>
+    public enum PluginResult
+    {
+        None = 0,
+
+        /// <summary>
+        /// 処理が正常に終了したことを表します。
+        /// </summary>
+        Succeeded = 1,
+
+        /// <summary>
+        /// ユーザにより処理がキャンセルされたことを表します。
+        /// </summary>
+        Cancelled = 2,
+
+        /// <summary>
+        /// 処理が正常に終了せず、中断したことを表します。
+        /// </summary>
+        Aborted = 3
+    }
+}
