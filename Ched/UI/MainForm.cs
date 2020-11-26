d044136d96eb070bc7af5ad1717310aff5fd5ff6
Paralleltree <paralleltree@outlook.com>
2020-11-26 16:43:22 +0900

    Implement showing DiagnosticsWindow

--- Ched/UI/MainForm.cs
+++ Ched/UI/MainForm.cs
@@ -326,17 +326,25 @@ namespace Ched.UI
             try
             {
                 var (result, args) = exportFunc();
-                // TODO: Diagnostics View
+                var vm = new DiagnosticsViewViewModel()
+                {
+                    Diagnostics = new System.Collections.ObjectModel.ObservableCollection<Diagnostic>(args.Diagnostics.ToList())
+                };
+                var window = new DiagnosticsWindow()
+                {
+                    DataContext = vm
+                };
                 switch (result)
                 {
                     case PluginResult.Succeeded:
-                        MessageBox.Show(this, ErrorStrings.ExportComplete, Program.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        vm.Message = ErrorStrings.ExportComplete;
                         break;
 
                     case PluginResult.Aborted:
-                        MessageBox.Show(this, ErrorStrings.ExportFailed, Program.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        vm.Message = ErrorStrings.ExportFailed;
                         break;
                 }
+                window.ShowDialog();
             }
             catch (Exception ex)
             {
